5324392864ce1e69ac39516667b423508c9da746
Xhall1 <gridoracones@gmail.com>
2025-07-25 00:13:40 -0500

    feat: custom hook useBoard and logic

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -2,12 +2,12 @@ import "./Tetris.css"
 
 import Board from "./Board"
 
-
+import { useBoard } from "../hooks/useBoard"
 
 const Tetris = ({ rows, columns, setGameOver }) => {
-
+    const [board, setBoard] = useBoard({rows, columns })
     return (
-       <Board />
+       <Board board={board}/>
     )
 }
 
