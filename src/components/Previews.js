a816e80a6f64444a1b813a398a826d679f4e72f9
Xhall1 <gridoracones@gmail.com>
2025-07-26 22:17:08 -0500

    feat: build the game pieces

--- src/components/Previews.js
+++ src/components/Previews.js
@@ -3,18 +3,17 @@ import React from "react"
 import Preview from "./Preview"
 
 const Previews = ({ tetrominoes }) => {
-    
-    const previewTetrominoes = tetrominoes
+  const previewTetrominoes = tetrominoes
     .slice(1 - tetrominoes.length)
-    .reverse()
+    .reverse();
 
-    return (
-        <>
-            {previewTetrominoes.map((tetromino, index) => (
-                <Preview tetromino={tetromino} index={index} key={index} />
-            ))}
-        </>
-    )
-}
+  return (
+    <>
+      {previewTetrominoes.map((tetromino, index) => (
+        <Preview tetromino={tetromino} index={index} key={index} />
+      ))}
+    </>
+  );
+};
 
-export default React.memo(Previews)
+export default React.memo(Previews);
--- src/hooks/useBoard.js
+++ src/hooks/useBoard.js
@@ -1,8 +1,26 @@
-import { useState } from "react"
-import { buildBoard } from "../business/Board"
+import { useState, useEffect } from "react";
 
-export const useBoard = ({ rows, columns }) => {
-    const  [board] = useState (buildBoard({ rows, columns }))
+import { buildBoard, nextBoard } from "../business/Board";
 
-    return [board]
-}
+export const useBoard = ({
+  rows,
+  columns,
+  player,
+  resetPlayer,
+  addLinesCleared
+}) => {
+  const [board, setBoard] = useState(buildBoard({ rows, columns }));
+
+  useEffect(() => {
+    setBoard((previousBoard) =>
+      nextBoard({
+        board: previousBoard,
+        player,
+        resetPlayer,
+        addLinesCleared
+      })
+    );
+  }, [player, resetPlayer, addLinesCleared]);
+
+  return [board];
+};
